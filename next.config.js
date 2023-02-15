efdd8bc9609472bf11a0be342dc632131e89ceb2
C. Lewis <ctj.lewis@icloud.com>
2023-02-14 21:32:17 -0600

    chore: leave `robots.txt`

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@
  */
 const config = {
   trailingSlash: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/:path*",
+        destination: "/docs/:path*",
+        permanent: true,
+      },
+    ];
+  }
 };
 
 export default config;
