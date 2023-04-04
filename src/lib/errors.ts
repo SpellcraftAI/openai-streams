9a1857a24a63febb7b3455544613dfca659441ab
neo773 <62795688+neo773@users.noreply.github.com>
2023-04-04 06:24:32 +0530

    Add missing error handling (#15)
    * Add missing error handling
    
    * Remove duplicate code

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -2,6 +2,12 @@ export const OpenAIErrors = {
   "NO_API_KEY": "No API key provided. Please set the OPENAI_API_KEY environment variable or pass the { apiKey } option.",
   "MAX_TOKENS": "Maximum number of tokens reached.",
   "UNKNOWN": "An unknown error occurred.",
+  "INVALID_API_KEY":  "Incorrect API key provided. You can find your API key at https://platform.openai.com/account/api-keys.",
+  "INVALID_MODEL":  "The model does not exist",
+  "RATE_LIMIT_REACHED": "You are sending requests too quickly. Pace your requests. Read the Rate limit guide.",
+  "EXCEEDED_QUOTA": "You have hit your maximum monthly spend (hard limit) which you can view in the account billing section. Apply for a quota increase.",
+  "ENGINE_OVERLOAD": "Our servers are experiencing high traffic. Please retry your requests after a brief wait.",
+  "SERVER_ERROR": "Issue on our servers. Retry your request after a brief wait and contact us if the issue persists. Check the status page.",
 } as const;
 
 export type OpenAIErrorType = keyof typeof OpenAIErrors;
