c92b9e10e52fa65e52cd23e238c3bfd68bbd760a
Calum Bird <calum@trelent.net>
2023-04-22 15:53:53 -0300

    Add apiBase option

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,35 +1,46 @@
-import type { CreateChatCompletionRequest, CreateCompletionRequest, CreateEditRequest, CreateEmbeddingRequest, CreateFineTuneRequest, CreateImageRequest } from "./pinned";
+import type {
+  CreateChatCompletionRequest,
+  CreateCompletionRequest,
+  CreateEditRequest,
+  CreateEmbeddingRequest,
+  CreateFineTuneRequest,
+  CreateImageRequest,
+} from "./pinned";
 
 export type StreamMode = "raw" | "tokens";
 
 export const OpenAIAPIEndpoints = {
-  "chat": "chat/completions",
-  "completions": "completions",
-  "edits": "edits",
-  "embeddings": "embeddings",
-  "images": "images",
-  "fine-tunes": "fine-tunes"
+  chat: "chat/completions",
+  completions: "completions",
+  edits: "edits",
+  embeddings: "embeddings",
+  images: "images",
+  "fine-tunes": "fine-tunes",
 } as const;
 
 export type OpenAIAPIEndpoint = keyof typeof OpenAIAPIEndpoints;
 
-export type OpenAICreateArgs<T extends OpenAIAPIEndpoint> = (
+export type OpenAICreateArgs<T extends OpenAIAPIEndpoint> =
   T extends "completions"
     ? Omit<CreateCompletionRequest, "stream">
     : T extends "edits"
-      ? CreateEditRequest
-      : T extends "embeddings"
-        ? CreateEmbeddingRequest
-        : T extends "images"
-          ? CreateImageRequest
-          : T extends "fine-tunes"
-            ? CreateFineTuneRequest
-            : T extends "chat"
-              ? CreateChatCompletionRequest
-              : never
-);
+    ? CreateEditRequest
+    : T extends "embeddings"
+    ? CreateEmbeddingRequest
+    : T extends "images"
+    ? CreateImageRequest
+    : T extends "fine-tunes"
+    ? CreateFineTuneRequest
+    : T extends "chat"
+    ? CreateChatCompletionRequest
+    : never;
 
 export type OpenAIOptions = {
+  /**
+   * By default, the API base is https://api.openai.com/v1, corresponding
+   * to OpenAIs API. You can override this to use a different provider or proxy.
+   */
+  apiBase?: string;
   /**
    * By default, the API key is read from the OPENAI_API_KEY environment
    * variable. You can override this by passing a different key here.
@@ -49,13 +60,11 @@ export type OpenAIOptions = {
 /**
  * The OpenAI API client for Edge runtime.
  */
-export type OpenAIEdgeClient = (
-  <T extends OpenAIAPIEndpoint>(
-    endpoint: T,
-    args: OpenAICreateArgs<T>,
-    options?: OpenAIOptions
-  ) => Promise<ReadableStream<Uint8Array>>
-);
+export type OpenAIEdgeClient = <T extends OpenAIAPIEndpoint>(
+  endpoint: T,
+  args: OpenAICreateArgs<T>,
+  options?: OpenAIOptions
+) => Promise<ReadableStream<Uint8Array>>;
 
 export type OpenAINodeClient = <T extends OpenAIAPIEndpoint>(
   endpoint: T,
