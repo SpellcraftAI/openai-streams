f26f66c836b281bd99ee695e3c719a19bfaf0fa8
Lewis <lewis@spellcraft.org>
2023-06-20 07:19:51 -0500

    fix: restrict pinned `openai` types (named only)
    Older TS versions throw for `export type *`.
    
    Fixes https://github.com/SpellcraftAI/openai-streams/issues/48.

--- src/lib/pinned.ts
+++ src/lib/pinned.ts
@@ -1,1 +1,8 @@
-export type * from "openai";
+export type {
+  CreateChatCompletionRequest,
+  CreateCompletionRequest,
+  CreateEditRequest,
+  CreateEmbeddingRequest,
+  CreateFineTuneRequest,
+  CreateImageRequest,
+} from "openai";
